0a353f7f41bc3c15f77f59f5db308e3e27e6974e
pratik-1 <nandekarpratik04@gmail.com>
2023-10-05 19:19:16 +0100

    Add graphql general and custom query

---
+++ backend/movies/schema.py
@@ -0,0 +1,30 @@
+import graphene
+from graphene_django.types import DjangoObjectType
+from .models import Movie
+
+
+class MovieType(DjangoObjectType):
+    class Meta:
+        model = Movie
+
+
+class Query(graphene.ObjectType):
+    all_movies = graphene.List(MovieType)
+    movies = graphene.Field(
+        MovieType, id=graphene.Int(), title=graphene.String()
+    )
+
+    def resolve_all_movies(self, info, **kwargs):
+        return Movie.objects.all()
+
+    def resolve_movies(self, info, **kwargs):
+        id = kwargs.get("id")
+        title = kwargs.get("title")
+        if id is not None:
+            return Movie.objects.get(pk=id)
+        if title is not None:
+            return Movie.objects.get(title=title)
+        return None
+
+
+schema = graphene.Schema(query=Query)
